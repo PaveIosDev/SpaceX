97c37d169eb9d90d7846abee160d52e56b4951d3
Pavelweb22 <pavelweb2016@yandex.ru>
2022-12-09 20:20:28 +0300

    Project setup

---
+++ SpaceX/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Павел Яковенко on 09.12.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    private let falconHeavyView = FalconHeavyView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        setupViews()
+        setConstraints()
+    }
+
+    private func setupViews() {
+        view.addSubview(falconHeavyView)
+    }
+}
+
+extension MainViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+        
+            falconHeavyView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
+            falconHeavyView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0),  
+            falconHeavyView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            falconHeavyView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
