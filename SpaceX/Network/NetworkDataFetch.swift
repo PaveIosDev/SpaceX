83875466f8c454402dd185aa74c1ab9ea960baa6
Pavelweb22 <pavelweb2016@yandex.ru>
2023-01-22 19:21:34 +0300

    NetworkDataFetch

---
+++ SpaceX/Network/NetworkDataFetch.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkDataFetch.swift
+//  SpaceX
+//
+//  Created by Павел Яковенко on 22.01.2023.
+//
+
+import Foundation
+
+class NetworkDataFetch {
+    static let shared = NetworkDataFetch()
+    private init() {}
+    
+    
+}
